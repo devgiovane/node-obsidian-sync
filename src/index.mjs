9a9e3a5f9605bc4509b7fb46b2c0d8d458b9a3da
Giovane Santos Silva <giovanesantos1999@gmail.com>
2022-09-01 21:17:43 -0300

    add: add job clear in schedule :sparkles:

--- src/index.mjs
+++ src/index.mjs
@@ -5,7 +5,11 @@ import * as jobs from './jobs/jobs.mjs';
 dotenv.config();
 
 schedule.scheduleJob('30 12 * * *', async function () {
+    console.log('Sync', new Intl.DateTimeFormat('pt-BR', {
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+    }).format(new Date()));
     try {
+        await jobs.clear();
         await jobs.sync(
             process.env.OBSIDIAN_FOLDER_SYNC,
             process.env.GOOGLE_DRIVE_FOLDER_SYNC,
